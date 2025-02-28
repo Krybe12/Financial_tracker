171d8eda29c774b7c42ef0bee6b6eb187e9ce172
Krybe12 <Krybe120@gmail.com>
2025-02-28 19:37:09 +0100

    fix gitignore file

--- backend/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-DB_USER = "root"
-DB_PASSWORD = "password"
-DB_HOST = "localhost"
-DB_NAME = "mydatabase"
-
-SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
-SQLALCHEMY_TRACK_MODIFICATIONS = False
