beaa2db310eac842761e004d4ffbc8c08e7559af
Krybe12 <Krybe120@gmail.com>
2025-03-02 17:44:54 +0100

    add backend env link

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+
+	import { PUBLIC_BACKEND } from "$env/static/public";
+
+</script>
+<h1>Welcome to SvelteKit {PUBLIC_BACKEND}</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
---
+++ frontend/src/routes/about/+page.svelte
@@ -0,0 +1,1 @@
+<h1>ahojkyyy</h1>
